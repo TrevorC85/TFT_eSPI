643f8c2f642d3c58101c2dce7fa688085bc62867
TrevorC85 <39962518+TrevorC85@users.noreply.github.com>
2022-07-20 17:22:01 -0500

    Update User_Setup.h

--- User_Setup.h
+++ User_Setup.h
@@ -1,4 +1,26 @@
-//                            USER DEFINED SETTINGS
+#define ST7735_DRIVER // Define additional parameters below for this display
+#define TFT_WIDTH 128
+// #define TFT_WIDTH  240 // ST7789 240 x 240 and 240 x 320
+#define TFT_HEIGHT 160
+#define ST7735_GREENTAB
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS 5  // Chip select control pin
+#define TFT_DC 2 // Data Command control pin
+#define TFT_RST 4 // Reset pin (could connect to RST pin)
+#define LOAD_GLCD // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+#define LOAD_FONT2 // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+#define LOAD_FONT4 // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+#define LOAD_FONT6 // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+#define LOAD_FONT7 // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+#define LOAD_FONT8 // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+//#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
+#define LOAD_GFXFF // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+#define SMOOTH_FONT
+#define SPI_FREQUENCY 27000000 // Actually sets it to 26.67MHz =
+
+
+/* //                            USER DEFINED SETTINGS
 //   Set driver type, fonts to be loaded, pins used and SPI control method etc
 //
 //   See the User_Setup_Select.h file if you wish to be able to define multiple
@@ -348,7 +370,7 @@
 #define SPI_READ_FREQUENCY  20000000
 
 // The XPT2046 requires a lower SPI clock rate of 2.5MHz so we define that here:
-#define SPI_TOUCH_FREQUENCY  2500000
+#define SPI_TOUCH_FREQUENCY  2500000 */
 
 // The ESP32 has 2 free SPI ports i.e. VSPI and HSPI, the VSPI is the default.
 // If the VSPI port is in use and pins are not accessible (e.g. TTGO T-Beam)
